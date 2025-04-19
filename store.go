e10d740222185d1f23a9628fd9b6c7921587b94f
Dhairya3124 <dk9582671597@gmail.com>
2025-04-19 17:02:38 +0530

    feat: added functions to create and get articles

--- store.go
+++ store.go
@@ -1,6 +1,9 @@
 package main
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type Store struct {
 	db *sql.DB
@@ -19,5 +22,42 @@ func (s *Store) Init() (*sql.DB, error) {
 		return nil, err
 	}
 	return db, nil
+
+}
+func (s *Store) Create(ctx context.Context, article *Article) error {
+	query := `
+	INSERT INTO articles(title,description,url)
+	VALUES($1,$2,$3)
+	RETURNING id, created_at
+	`
+	err := s.db.QueryRowContext(ctx, query,
+		article.Title,
+		article.Description,
+		article.URL).Scan(&article.ID, article.CreatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+func (s *Store) GetArticles(ctx context.Context) ([]Article, error) {
+	query := `
+	SELECT * FROM articles
+	`
+	rows, err := s.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	articles := []Article{}
+	for rows.Next() {
+		var a Article
+		err := rows.Scan(&a.ID, &a.CreatedAt, &a.Title, &a.Description, &a.URL)
+		if err != nil {
+			return nil, err
+		}
+		articles = append(articles, a)
+	}
+	return articles, nil
 
 }
